907d382fb9c4ce5357f654ecd5a725f5bab5b2fa
arlovaalina <arlovaalina@gmail.com>
2019-02-17 18:40:21 +0300

    add react app configuration

---
+++ web/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.pcss$/,
+        loaders: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.pcss', '.css', '.js', '.jsx'],
+  },
+};
