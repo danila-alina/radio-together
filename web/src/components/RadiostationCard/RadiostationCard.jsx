6b7f69308bbee6eb1afeea9d1cb5556ea7b3c1bc
arlovaalina <arlovaalina@gmail.com>
2019-06-10 08:54:54 +0300

    Add click on radiostation handler

--- web/src/components/RadiostationCard/RadiostationCard.jsx
+++ web/src/components/RadiostationCard/RadiostationCard.jsx
@@ -3,24 +3,26 @@ import PropTypes from 'prop-types';
 
 import Spectrum from 'components/Spectrum';
 
-import {
-  Card, UserName, RadiostationName, Info,
-} from './RadiostationCard.styled';
+import * as SC from './RadiostationCard.styled';
 
 class RadiostationCard extends React.Component {
   render() {
-    const { colors, userName, radiostationName } = this.props;
+    const {
+      colors, userName, radiostationName, userId,
+    } = this.props;
     return (
-      <Card
-        topColor={colors.top}
-        bottomColor={colors.bottom}
-      >
-        <Spectrum />
-        <Info>
-          <UserName>{userName}</UserName>
-          <RadiostationName>{radiostationName}</RadiostationName>
-        </Info>
-      </Card>
+      <SC.CardContainer to="/my-account">
+        <SC.Card
+          topColor={colors.top}
+          bottomColor={colors.bottom}
+        >
+          <Spectrum />
+          <SC.Info>
+            <SC.UserName>{userName}</SC.UserName>
+            <SC.RadiostationName>{radiostationName}</SC.RadiostationName>
+          </SC.Info>
+        </SC.Card>
+      </SC.CardContainer>
     );
   }
 }
@@ -29,6 +31,11 @@ RadiostationCard.propTypes = {
   colors: PropTypes.objectOf(PropTypes.string).isRequired,
   userName: PropTypes.string.isRequired,
   radiostationName: PropTypes.string.isRequired,
+  userId: PropTypes.string,
+};
+
+RadiostationCard.defaultProps = {
+  userId: '',
 };
 
 export default RadiostationCard;
--- web/src/components/RadiostationCard/RadiostationCard.styled.js
+++ web/src/components/RadiostationCard/RadiostationCard.styled.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 import * as styles from 'constants/styles';
+import { NavLink } from 'react-router-dom';
+
+export const CardContainer = styled(NavLink)`
+  text-decoration: none;
+  margin-right: 30px;
+`;
 
 export const Card = styled.div`
   width: 130px;
   height: 130px;
   border-radius: 10px;
   background: linear-gradient(${props => props.topColor}, ${props => props.bottomColor});
-  margin-right: 30px;
   padding: 5px;
   position: relative;
   z-index: 0;
